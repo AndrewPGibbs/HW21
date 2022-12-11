1b57ea33531ed3ec3cfb72445bbfe71b03950029
Andrew PG <andrewpgibbs71@gmail.com>
2022-12-11 18:05:56 -0500

    adding typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,13 +2,19 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   # Define which fields are accessible from the Class model
-  type Class {
+  type User {
     _id: ID
     username: String
     email: String
-    unqique: true
     password: String
-    savedBooks: [booksSchema]
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
   }
 
   # Define which queries the front end is allowed to make and what data is returned
